7e7e19965654af3731e6628e6187b3b62f8cd863
Michele Iacobone <michele.iacobone@gmail.com>
2015-11-12 21:16:20 +0100

    $(uptime) output doesn't have constant format => awk-based solution works half the time. back to bash!

--- uptimer.sh
+++ uptimer.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-echo $(uptime) $(cat $1) |
-tr : ' ' | tr , ' ' | awk '
-{
-  dd=$5
-  hh=$7
-  mm=$8
-  current_uptime=dd*24*60+hh*60+mm
-  if (current_uptime>$19) print current_uptime
-  else print $19
-}	
-'
+ut=$(uptime --pretty) #shorter
+dd=$(echo "$ut" | grep --only-matching "[0-9]* day" | cut -d " " -f 1)
+hh=$(echo "$ut" | grep --only-matching "[0-9]* hour" | cut -d " " -f 1)
+mm=$(echo "$ut" | grep --only-matching "[0-9]* minute" | cut -d " " -f 1)
+
+#handle particular cases in the string above. e.g. "3 days, 10 minutes" => no hours.
+[[ -n $dd ]] || dd="0"
+[[ -n $hh ]] || hh="0"
+[[ -n $mm ]] || mm="0"
+
+#compute stuff
+current_uptime=$(echo "$dd*24*60+$hh*60+$mm" | bc)
+best_uptime=$(cat $1 2>/dev/null) || best_uptime="0" #file might not exist yet!
+
+#and the winner is...
+if [ $current_uptime -gt $best_uptime ]
+then
+	echo $current_uptime
+else
+	echo $best_uptime
+fi
